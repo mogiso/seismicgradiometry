d397a2fd50dbb7f25b095dae806f71eedd64d268
Masashi Ogiso <mogiso@mri-jma.go.jp>
2022-03-08 15:10:26 +0900

    initial commit

---
+++ sort.F90
@@ -0,0 +1,29 @@
+module sort
+  use nrtype, only : fp
+  private
+  public :: bubblesort
+
+contains
+
+  subroutine bubblesort(data)
+    implicit none
+    real(kind = fp), intent(inout)  :: data(:)
+    integer :: i, j, n
+    real(kind = fp) :: tmp
+ 
+    n = size(data)
+    do j = 1, n - 1
+      do i = 2, n - j + 1
+        if(data(i) .gt. data(i - 1)) then
+          tmp = data(i)
+          data(i) = data(i - 1)
+          data(i - 1) = tmp
+        endif
+      enddo
+    enddo
+  
+    return 
+  end subroutine bubblesort
+
+end module sort
+

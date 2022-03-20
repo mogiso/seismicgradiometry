ba5cdf464f45647b3860eb8a8ce35063b2067ffa
Masashi Ogiso <mogiso@mri-jma.go.jp>
2022-03-20 16:27:09 +0900

    bug fix: take inverse in weighted matrix

--- seismicgradiometry.F90
+++ seismicgradiometry.F90
@@ -200,7 +200,7 @@ program seismicgradiometry
         !else
         !  weight(i, i) = 0.0_fp
         !endif
-        weight(i, i) = (dist_x_tmp ** 2 + dist_y_tmp ** 2) / 10.0_fp
+        weight(i, i) = 1.0_fp / ((dist_x_tmp ** 2 + dist_y_tmp ** 2) / 10.0_fp)
       enddo
       g_tmp = matmul(transpose(g), matmul(weight, g))
 #ifdef MKL

3a5205538a7eba3c71be22c6b884cddc26b9ebd0
Masashi Ogiso <mogiso@mri-jma.go.jp>
2022-03-28 14:48:46 +0900

    fix bug in -R, plot relative error of velocity

--- plot_velocity_err_gradiometry.pl
+++ plot_velocity_err_gradiometry.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 use Math::Trig qw(pi rad2deg);
 use Parallel::ForkManager;
-$MAX_PROCESS = 1;
+$MAX_PROCESS = 16;
 
 $in_dir = $ARGV[0];
 $index_begin = $ARGV[1];
@@ -19,24 +19,24 @@ $dt = 60;
 $ref_sec = $ref_hh * 3600 + $ref_mm * 60 + $ref_ss;
 
 #S-net ref: 142.5E, 38.25N
-$dgrid_x = 20;
-$dgrid_y = 20;
-$min_x = -350;
-$max_x = 350;
-$min_y = -600;
-$max_y = 600;
-$size_x = 7;
-$size_y = 12;
+#$dgrid_x = 20;
+#$dgrid_y = 20;
+#$min_x = -350;
+#$max_x = 350;
+#$min_y = -600;
+#$max_y = 600;
+#$size_x = 7;
+#$size_y = 12;
 
 #DONET ref: 135.75E, 33.2N
-#$dgrid_x = 10;
-#$dgrid_y = 10;
-#$min_x = -150;
-#$max_x = 150;
-#$min_y = -100;
-#$max_y = 100;
-#$size_x = 7;
-#$size_y = 4.7;
+$dgrid_x = 10;
+$dgrid_y = 10;
+$min_x = -150;
+$max_x = 150;
+$min_y = -100;
+$max_y = 100;
+$size_x = 7;
+$size_y = 4.7;
 
 $dx = $size_x + 0.8;
 
@@ -52,7 +52,7 @@ $txt_y = $size_y - 0.15;
 $cpt_x = $size_x / 2.0;
 $cpt_y = -1.7;
 $cpt_len = $size_x;
-$cpt_width = "0.3ch";
+$cpt_width = "0.3";
 $slowness_length = 0.35;
 $decimate = 1;
 
@@ -111,9 +111,9 @@ foreach $index (@index_array){
       read IN, $buf, 4;
       push @slowness_y, (unpack "f", $buf);
       read IN, $buf, 4;
-      push @sigma_slowness_x, ((unpack "f", $buf));
+      push @sigma_slowness_x, (unpack "f", $buf);
       read IN, $buf, 4;
-      push @sigma_slowness_y, ((unpack "f", $buf));
+      push @sigma_slowness_y, (unpack "f", $buf);
       read IN, $buf, 4;
       push @ampterm_x, (unpack "f", $buf);
       read IN, $buf, 4;
@@ -136,15 +136,17 @@ foreach $index (@index_array){
   }
 
   if (-f $in2){
-    open OUT, " | gmt psxy -JX$size_x/$size_y -R$min_x/$max_y/$min_y/$max_y -Ss$symbolsize -W0.5p,black \\
+    open OUT, " | gmt psxy -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -Ss$symbolsize -W0.5p,black \\
                            -C$cpt_vel -O -K -P >> $out";
     for($i = 0; $i <= $#x_east; $i++){
       $slowness_sq = ($slowness_x[$i] ** 2 + $slowness_y[$i] ** 2);
-      $velocity_err = 1000.0 / $slowness_sq * 
+      $velocity = sqrt(1.0 / $slowness_sq) * 1000.0;
+        $velocity_err = 1000.0 / $slowness_sq * 
                       sqrt(($sigma_slowness_x[$i] ** 2 * $slowness_x[$i] ** 2 + $sigma_slowness_y[$i] ** 2 * $slowness_y[$i] ** 2)
                            / $slowness_sq);
-      print OUT "$x_east[$i] $y_north[$i] $velocity_err\n";
-      #print stdout "$x_east[$i] $y_north[$i] $velocity_err\n";
+        $velocity_err = $velocity_err / $velocity * 100.0;
+        print OUT "$x_east[$i] $y_north[$i] $velocity_err\n";
+      #print stdout "$x_east[$i] $y_north[$i] $velocity_err $velocity\n";
     }
     close OUT;
   }
@@ -155,7 +157,8 @@ foreach $index (@index_array){
   close OUT;
 
 
-  system "gmt psscale -Dx$cpt_x/$cpt_y/$cpt_len/$cpt_width -B+l\"velocity error (m/s)\" -C$cpt_vel -O -K -P >> $out";
+  system "gmt psscale -Dx$cpt_x/$cpt_y/$cpt_len/${cpt_width}ch -Ef \\
+                      -Ba10+l\"Relative error of velocity (\%)\" -C$cpt_vel -O -K -P >> $out";
 
   system "cat /dev/null | gmt psxy -JX1c -R0/1/0/1 -Sc0.1 -O -K -P -X$dx >> $out";
 
@@ -166,7 +169,7 @@ foreach $index (@index_array){
   }
 
   if (-f $in2){
-    open OUT, " | gmt psxy -JX$size_x/$size_y -R$min_x/$max_y/$min_y/$max_y -Ss$symbolsize -W0.5p,black \\
+    open OUT, " | gmt psxy -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -Ss$symbolsize -W0.5p,black \\
                            -C$cpt_az -O -K -P >> $out";
     for($i = 0; $i <= $#x_east; $i++){
       $slowness_sq = $slowness_x[$i] ** 2 + $slowness_y[$i] ** 2;
@@ -184,7 +187,7 @@ foreach $index (@index_array){
     print OUT "$txt_x3 $txt_y2 14p,Helvetica,black 0 LB (b)\n";
   close OUT;
 
-  system "gmt psscale -Dx$cpt_x/$cpt_y/$cpt_len/$cpt_width -B+l\"Azimuth error (degree)\" -C$cpt_az -O -K -P >> $out";
+  system "gmt psscale -Dx$cpt_x/$cpt_y/$cpt_len/${cpt_width}ch -Ef -B+l\"Azimuth error (\\232)\" -C$cpt_az -O -K -P >> $out";
 
   system "cat /dev/null | gmt psxy -JX1c -R0/1/0/1 -Sc0.1 -O -P >> $out";
   system "gmt psconvert $out -Tg -A";

dba07781bb2c2ad9aff05da9f0227b014ed5b1cc
Masashi Ogiso <mogiso@mri-jma.go.jp>
2023-04-26 15:22:22 +0900

    commit temporally

--- gradiometry_parameters.F90
+++ gradiometry_parameters.F90
@@ -61,10 +61,10 @@ module gradiometry_parameters
   &                                     x_end = 205.0_fp, y_end = 225.0_fp
   real(kind = fp), public, parameter :: center_lon = 139.0_fp, center_lat = 36.0_fp
   real(kind = fp), public, parameter :: dgrid_x = 2.5_fp, dgrid_y = 2.5_fp
-  real(kind = fp), public, parameter :: cutoff_dist = 10.0_fp
+  real(kind = fp), public, parameter :: cutoff_dist = 7.0_fp
   integer,         public, parameter :: nsta_grid_max = 10, nsta_grid_min = 3
   integer,         public, parameter :: ntime_slowness = 60, ntime_slowness2 = (ntime_slowness - 1) / 2
-  real(kind = fp), public, parameter :: fl = 1.0_fp / 10.0_fp, fh = 1.0_fp / 5.0_fp, fs = fh * 2.0_fp, &
+  real(kind = fp), public, parameter :: fl = 1.0_fp / 20.0_fp, fh = 1.0_fp / 10.0_fp, fs = fh * 2.0_fp, &
   &                                     ap = 0.5_fp, as = 10.0_fp
   integer,         public, parameter :: ntime = 27000
 #ifdef ELLIPSE
@@ -83,9 +83,9 @@ module gradiometry_parameters
   real(kind = fp), public, parameter :: xcorr_min = 0.7_fp
 
   integer,         public, parameter :: ntimestep = 100
-  integer,         public, parameter :: naddstation_array = 1
-  integer,         public, parameter :: niteration_max = 5
-  integer,         public, parameter :: ngradient = 1000, ngradient2 = ngradient / 2, ngradient4 = ngradient / 4
+  integer,         public, parameter :: naddstation_array = 0
+  integer,         public, parameter :: niteration_max = 8 
+  integer,         public, parameter :: ngradient = 1000, ngradient2 = ngradient / 2, ngradient4 = ngradient2 / 2 + 50
 
   !!for AELUMA method
   integer,         public, parameter :: ntime_fft = 1024

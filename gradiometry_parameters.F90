226c6377f23ed636ba1dacaad4bf2e5fbd75266b
Masashi Ogiso <mogiso@mri-jma.go.jp>
2023-05-26 20:46:58 +0900

    commit temporally

--- gradiometry_parameters.F90
+++ gradiometry_parameters.F90
@@ -10,21 +10,29 @@ module gradiometry_parameters
   private
 
   !!S-net OBP long-period (20-60min.)
-  !real(kind = fp), public, parameter :: order = 1.0e-2_fp !!Pa -> hpa
-  !real(kind = fp), public, parameter :: az_diff_max = 150.0_fp * deg2rad
-  !real(kind = fp), public, parameter :: x_start = -350.0_fp, y_start = -600.0_fp, &
-  !&                                     x_end = 350.0_fp, y_end = 600.0_fp
-  !real(kind = fp), public, parameter :: center_lon = 142.5_fp, center_lat = 38.25_fp 
-  !real(kind = fp), public, parameter :: dgrid_x = 20.0_fp, dgrid_y = 20.0_fp
-  !real(kind = fp), public, parameter :: cutoff_dist = 80.0_fp
-  !real(kind = fp), public, parameter :: fl = 1.0_fp / (60.0_fp * 60.0_fp), fh = 1.0_fp / (20.0_fp * 60.0_fp), &
-  !&                                     fs = 1.0_fp / (10.0_fp * 60.0_fp), ap = 0.5_fp, as = 5.0_fp
+  real(kind = fp), public, parameter :: eps = 1.0e-10_fp
+  real(kind = fp), public, parameter :: order = 1.0e-2_fp !!Pa -> hpa
+  real(kind = fp), public, parameter :: az_diff_max = 150.0_fp * deg2rad
+  real(kind = fp), public, parameter :: x_start = -350.0_fp, y_start = -600.0_fp, &
+  &                                     x_end = 350.0_fp, y_end = 600.0_fp
+  real(kind = fp), public, parameter :: center_lon = 142.5_fp, center_lat = 38.25_fp 
+  real(kind = fp), public, parameter :: dgrid_x = 10.0_fp, dgrid_y = 10.0_fp
+  real(kind = fp), public, parameter :: cutoff_dist = 80.0_fp
+  real(kind = fp), public, parameter :: fl = 1.0_fp / (60.0_fp * 60.0_fp), fh = 1.0_fp / (20.0_fp * 60.0_fp), &
+  &                                     fs = 1.0_fp / (10.0_fp * 60.0_fp), ap = 0.5_fp, as = 5.0_fp
   !integer,         public, parameter :: ntime_slowness = 61, ntime_slowness2 = (ntime_slowness - 1) / 2
   !integer,         public, parameter :: nsta_grid_max = 40, nsta_grid_min = 5  !!For S-net/DONET OBPG array
+  integer,         public, parameter ::  nsta_grid_min = 4  !!For S-net/DONET OBPG array
+  integer,         public, parameter ::  nsta_grid_max = 4  !!For S-net/DONET OBPG array
   !integer,         public, parameter :: ntime = 630
   !integer,         public, parameter :: ntime = 1024 !!testdata
+  integer,         public, parameter :: ntime = 6000 !!testdata
+  integer,         public, parameter :: ntimestep = 10
+  integer,         public, parameter :: naddstation_array = 1
+  integer,         public, parameter :: ngradient2 = 600
 
   !!DONET OBP long-period (20-60min.)
+  !real(kind = fp), public, parameter :: eps = 1.0e-10_fp
   !real(kind = fp), public, parameter :: order = 1.0e-2_fp  !!Pa -> hpa
   !real(kind = fp), public, parameter :: az_diff_max = 150.0_fp * deg2rad
   !real(kind = fp), public, parameter :: x_start = -150.0_fp, y_start = -100.0_fp, &
@@ -40,6 +48,7 @@ module gradiometry_parameters
   !integer,         public, parameter :: ntime = 1024 !!testdata
 
   !!DONET OBP short-period (6-20min.)
+  !real(kind = fp), public, parameter :: eps = 1.0e-10_fp
   !real(kind = fp), public, parameter :: order = 1.0e-2_fp  !!Pa -> hpa
   !real(kind = fp), public, parameter :: az_diff_max = 150.0_fp * deg2rad
   !real(kind = fp), public, parameter :: x_start = -150.0_fp, y_start = -100.0_fp, &
@@ -55,40 +64,40 @@ module gradiometry_parameters
   !integer,        public, parameter :: ntime = 1024 !!testdata
 
   !!SK-net long-period ground motion (5-10s)
-  real(kind = fp), public, parameter :: order = 1.0e-6_fp  !!nm -> mm
-  real(kind = fp), public, parameter :: az_diff_max = 150.0_fp * deg2rad
-  real(kind = fp), public, parameter :: x_start = -250.0_fp, y_start = -250.0_fp, &
-  &                                     x_end = 205.0_fp, y_end = 225.0_fp
-  real(kind = fp), public, parameter :: center_lon = 139.0_fp, center_lat = 36.0_fp
-  real(kind = fp), public, parameter :: dgrid_x = 2.5_fp, dgrid_y = 2.5_fp
-  real(kind = fp), public, parameter :: cutoff_dist = 7.0_fp
-  integer,         public, parameter :: nsta_grid_max = 10, nsta_grid_min = 3
-  integer,         public, parameter :: ntime_slowness = 60, ntime_slowness2 = (ntime_slowness - 1) / 2
-  real(kind = fp), public, parameter :: fl = 1.0_fp / 20.0_fp, fh = 1.0_fp / 10.0_fp, fs = fh * 2.0_fp, &
-  &                                     ap = 0.5_fp, as = 10.0_fp
-  integer,         public, parameter :: ntime = 27000
+  !real(kind = fp), public, parameter :: eps = 1.0e-6_fp
+  !real(kind = fp), public, parameter :: order = 1.0e-6_fp  !!nm -> mm
+  !real(kind = fp), public, parameter :: az_diff_max = 150.0_fp * deg2rad
+  !real(kind = fp), public, parameter :: x_start = -250.0_fp, y_start = -250.0_fp, &
+  !&                                     x_end = 205.0_fp, y_end = 225.0_fp
+  !real(kind = fp), public, parameter :: center_lon = 139.0_fp, center_lat = 36.0_fp
+  !real(kind = fp), public, parameter :: dgrid_x = 2.5_fp, dgrid_y = 2.5_fp
+  !real(kind = fp), public, parameter :: cutoff_dist = 20.0_fp
+  !integer,         public, parameter :: nsta_grid_max = 10, nsta_grid_min = 4
+  !integer,         public, parameter :: ntime_slowness = 60, ntime_slowness2 = (ntime_slowness - 1) / 2
+  !real(kind = fp), public, parameter :: fl = 1.0_fp / 10.0_fp, fh = 1.0_fp / 5.0_fp, fs = fh * 2.0_fp, &
+  !&                                     ap = 0.5_fp, as = 10.0_fp
+  !integer,         public, parameter :: ntime = 18000
+  !integer,         public, parameter :: ntimestep = 100
+  !integer,         public, parameter :: naddstation_array = 1
+  !integer,         public, parameter :: ngradient2 = 1500, ngradient4 = ngradient2 / 2
 #ifdef ELLIPSE
   !!sigma_x: normal to propagation direction, sigma_y: parallel to propagation direction
   real(kind = fp), public, parameter :: sigma_x = cutoff_dist * 2.0_fp, sigma_y = cutoff_dist
   real(kind = fp), public, parameter :: evlon = 137.8910_fp, evlat = 36.6928_fp
 #endif
 
+  !!threshold of interstation distance (vertix distance) for delaunay triangulation
+  real(kind = fp), public, parameter :: interstationdistance_min = 0.05_fp
 
   !!common constants
   integer,         public, parameter :: ngrid_x = int((x_end - x_start) / dgrid_x) + 1
   integer,         public, parameter :: ngrid_y = int((y_end - y_start) / dgrid_y) + 1
-  !!threshold of interstation distance (vertix distance) for delaunay triangulation
-  real(kind = fp), public, parameter :: interstationdistance_min = 0.05_fp
-  real(kind = fp), public, parameter :: cos_taper_ratio = 0.1_fp
-  real(kind = fp), public, parameter :: xcorr_min = 0.7_fp
-
-  integer,         public, parameter :: ntimestep = 100
-  integer,         public, parameter :: naddstation_array = 0
-  integer,         public, parameter :: niteration_max = 8 
-  integer,         public, parameter :: ngradient = 1000, ngradient2 = ngradient / 2, ngradient4 = ngradient2 / 2 + 50
+  integer,         public, parameter :: niteration_max = 15
 
   !!for AELUMA method
   integer,         public, parameter :: ntime_fft = 1024
+  real(kind = fp), public, parameter :: xcorr_min = 0.7_fp
+  real(kind = fp), public, parameter :: cos_taper_ratio = 0.1_fp
  
 end module gradiometry_parameters
   

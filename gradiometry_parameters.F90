09dcfa9d8efe147800bd68d0eed6e8c5b97fbada
Masashi Ogiso <mogiso@mri-jma.go.jp>
2024-08-09 14:32:42 +0900

    add new variable for error estimation

--- gradiometry_parameters.F90
+++ gradiometry_parameters.F90
@@ -5,21 +5,21 @@
 
 module gradiometry_parameters
   use nrtype, only : fp
-  use constants, only : deg2rad
+  use constants, only : deg2rad, pi
   implicit none
   private
 
   real(kind = fp), public, parameter :: eps = 1.0e-10_fp
   !!S-net region
-  !real(kind = fp), public, parameter :: x_start = -350.0_fp, y_start = -600.0_fp, &
-  !&                                     x_end   = 350.0_fp,  y_end   = 600.0_fp
-  !real(kind = fp), public, parameter :: center_lon = 142.5_fp, center_lat = 38.25_fp 
-  !real(kind = fp), public, parameter :: dgrid_x = 20.0_fp, dgrid_y = 20.0_fp
+  real(kind = fp), public, parameter :: x_start = -350.0_fp, y_start = -600.0_fp, &
+  &                                     x_end   = 350.0_fp,  y_end   = 600.0_fp
+  real(kind = fp), public, parameter :: center_lon = 142.5_fp, center_lat = 38.25_fp 
+  real(kind = fp), public, parameter :: dgrid_x = 20.0_fp, dgrid_y = 20.0_fp
   !!DONET region
-  real(kind = fp), public, parameter :: x_start = -150.0_fp, y_start = -100.0_fp, &
-  &                                     x_end   = 150.0_fp,  y_end   = 100.0_fp
-  real(kind = fp), public, parameter :: center_lon = 135.75_fp, center_lat = 33.2_fp 
-  real(kind = fp), public, parameter :: dgrid_x = 10.0_fp, dgrid_y = 10.0_fp
+  !real(kind = fp), public, parameter :: x_start = -150.0_fp, y_start = -100.0_fp, &
+  !&                                     x_end   = 150.0_fp,  y_end   = 100.0_fp
+  !real(kind = fp), public, parameter :: center_lon = 135.75_fp, center_lat = 33.2_fp 
+  !real(kind = fp), public, parameter :: dgrid_x = 10.0_fp, dgrid_y = 10.0_fp
   !!SK-net long-period ground motion (5-10s)
   !real(kind = fp), public, parameter :: eps = 1.0e-6_fp
   !real(kind = fp), public, parameter :: x_start = -250.0_fp, y_start = -250.0_fp, &
@@ -28,15 +28,15 @@ module gradiometry_parameters
   !real(kind = fp), public, parameter :: dgrid_x = 2.5_fp, dgrid_y = 2.5_fp
 
   !!For 15s-sampled data 
-  !integer,         public, parameter :: ntime = 2400
-  !integer,         public, parameter :: ntimestep = 4 
-  !integer,         public, parameter :: ngradient2 = 240
-  !integer,         public, parameter :: ntime_slowness = 241
+  integer,         public, parameter :: ntime = 2400
+  integer,         public, parameter :: ntimestep = 4 
+  integer,         public, parameter :: ngradient2 = 240
+  integer,         public, parameter :: ntime_slowness = 241
   !!For 6s-sampled data
-  integer,         public, parameter :: ntime = 6000
-  integer,         public, parameter :: ntimestep = 10
-  integer,         public, parameter :: ngradient2 = 600
-  integer,         public, parameter :: ntime_slowness = 601, ntime_slowness2 = (ntime_slowness - 1) / 2
+  !integer,         public, parameter :: ntime = 6000
+  !integer,         public, parameter :: ntimestep = 10
+  !integer,         public, parameter :: ngradient2 = 600
+  !integer,         public, parameter :: ntime_slowness = 601, ntime_slowness2 = (ntime_slowness - 1) / 2
   !!For 0.01s-sampled data  (SK-net)
   !integer,         public, parameter :: ntime = 18000
   !integer,         public, parameter :: ntimestep = 100
@@ -61,22 +61,22 @@ module gradiometry_parameters
 #else
 
   !!data order, array configuration
-  !real(kind = fp), public, parameter :: order = 1.0e-2_fp !!Pa -> hPa
-  !integer,         public, parameter :: nsta_grid_min = 4, nsta_grid_max = 4
-  !integer,         public, parameter :: naddstation_array = 1
-  !real(kind = fp), public, parameter :: cutoff_dist = 80.0_fp !!S-net
+  real(kind = fp), public, parameter :: order = 1.0e-2_fp !!Pa -> hPa
+  integer,         public, parameter :: nsta_grid_min = 4, nsta_grid_max = 4
+  integer,         public, parameter :: naddstation_array = 1
+  real(kind = fp), public, parameter :: cutoff_dist = 80.0_fp !!S-net
   !!2023Philippines
-  real(kind = fp), public, parameter :: order = 1.0_fp !!2023Philippines: already hPa
-  integer,         public, parameter :: nsta_grid_min = 5, nsta_grid_max = 6
-  integer,         public, parameter :: naddstation_array = 3
-  real(kind = fp), public, parameter :: cutoff_dist = 60.0_fp  !!DONET
+  !real(kind = fp), public, parameter :: order = 1.0_fp !!2023Philippines: already hPa
+  !integer,         public, parameter :: nsta_grid_min = 4, nsta_grid_max = 6
+  !integer,         public, parameter :: naddstation_array = 3
+  !real(kind = fp), public, parameter :: cutoff_dist = 60.0_fp  !!DONET
   real(kind = fp), public, parameter :: az_diff_max = 150.0_fp * deg2rad
   !real(kind = fp), public, parameter :: fl = 1.0_fp / (60.0_fp * 60.0_fp), fh = 1.0_fp / (20.0_fp * 60.0_fp), &
   !&                                     fs = 1.0_fp / (10.0_fp * 60.0_fp), ap = 0.5_fp, as = 5.0_fp
-  !real(kind = fp), public, parameter :: fl = 1.0_fp / (60.0_fp * 60.0_fp), fh = 1.0_fp / (20.0_fp * 60.0_fp), &
-  !&                                     fs = 1.0_fp / (5.0_fp * 60.0_fp), ap = 0.5_fp, as = 5.0_fp
-  real(kind = fp), public, parameter :: fl = 1.0_fp / (180.0_fp * 60.0_fp), fh = 1.0_fp / (20.0_fp * 60.0_fp), &
+  real(kind = fp), public, parameter :: fl = 1.0_fp / (60.0_fp * 60.0_fp), fh = 1.0_fp / (10.0_fp * 60.0_fp), &
   &                                     fs = 1.0_fp / (5.0_fp * 60.0_fp), ap = 0.5_fp, as = 5.0_fp
+  !real(kind = fp), public, parameter :: fl = 1.0_fp / (180.0_fp * 60.0_fp), fh = 1.0_fp / (30.0_fp * 60.0_fp), &
+  !&                                     fs = 1.0_fp / (5.0_fp * 60.0_fp), ap = 0.5_fp, as = 5.0_fp
 
 #endif
 
@@ -90,6 +90,8 @@ module gradiometry_parameters
   !!Bandpass filter
   !real(kind = fp), public, parameter :: fl = 1.0_fp / 10.0_fp, fh = 1.0_fp / 5.0_fp, fs = fh * 2.0_fp, &
   !&                                     ap = 0.5_fp, as = 10.0_fp
+
+  real(kind = fp), public, parameter :: error_omega = 2.0_fp * pi * 1.0_fp / ((1.0_fp / fl + 1.0_fp / fh) * 0.5_fp)
 
 #ifdef ELLIPSE
   !!sigma_x: normal to propagation direction, sigma_y: parallel to propagation direction

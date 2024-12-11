0b7c3fdc35f98259804a7cdd67778da58549055b
Masashi Ogiso <mogiso@mri-jma.go.jp>
2024-12-11 20:23:46 +0900

    separate parameters for aeluma method

--- gradiometry_parameters.F90
+++ gradiometry_parameters.F90
@@ -10,40 +10,19 @@ module gradiometry_parameters
   private
 
   real(kind = fp), public, parameter :: eps = 1.0e-10_fp
+
+  
+#ifdef SORATENA
   !!S-net region
   real(kind = fp), public, parameter :: x_start = -350.0_fp, y_start = -600.0_fp, &
   &                                     x_end   = 350.0_fp,  y_end   = 600.0_fp
   real(kind = fp), public, parameter :: center_lon = 142.5_fp, center_lat = 38.25_fp 
   real(kind = fp), public, parameter :: dgrid_x = 20.0_fp, dgrid_y = 20.0_fp
-  !!DONET region
-  !real(kind = fp), public, parameter :: x_start = -150.0_fp, y_start = -100.0_fp, &
-  !&                                     x_end   = 150.0_fp,  y_end   = 100.0_fp
-  !real(kind = fp), public, parameter :: center_lon = 135.75_fp, center_lat = 33.2_fp 
-  !real(kind = fp), public, parameter :: dgrid_x = 10.0_fp, dgrid_y = 10.0_fp
-  !!SK-net long-period ground motion (5-10s)
-  !real(kind = fp), public, parameter :: eps = 1.0e-6_fp
-  !real(kind = fp), public, parameter :: x_start = -250.0_fp, y_start = -250.0_fp, &
-  !&                                     x_end   =  205.0_fp, y_end   =  225.0_fp
-  !real(kind = fp), public, parameter :: center_lon = 139.0_fp, center_lat = 36.0_fp
-  !real(kind = fp), public, parameter :: dgrid_x = 2.5_fp, dgrid_y = 2.5_fp
-
   !!For 15s-sampled data 
   integer,         public, parameter :: ntime = 2400
   integer,         public, parameter :: ntimestep = 4 
   integer,         public, parameter :: ngradient2 = 240
   integer,         public, parameter :: ntime_slowness = 241
-  !!For 6s-sampled data
-  !integer,         public, parameter :: ntime = 6000
-  !integer,         public, parameter :: ntimestep = 10
-  !integer,         public, parameter :: ngradient2 = 600
-  !integer,         public, parameter :: ntime_slowness = 601, ntime_slowness2 = (ntime_slowness - 1) / 2
-  !!For 0.01s-sampled data  (SK-net)
-  !integer,         public, parameter :: ntime = 18000
-  !integer,         public, parameter :: ntimestep = 100
-  !integer,         public, parameter :: ngradient2 = 1500
-  !integer,         public, parameter :: ntime_slowness = 60, ntime_slowness2 = (ntime_slowness - 1) / 2
-  
-#ifdef SORATENA
   !!data order, array configuration
   real(kind = fp), public, parameter :: order = 1.0_fp !!already hpa
   integer,         public, parameter :: nsta_grid_min = 15, nsta_grid_max = 23  !!For S-net/DONET OBPG array
@@ -58,9 +37,53 @@ module gradiometry_parameters
   !real(kind = fp), public, parameter :: fl = 1.0_fp / (180.0_fp * 60.0_fp), fh = 1.0_fp / (4.0_fp * 60.0_fp), &
   !&                                     fs = 1.0_fp / (3.0_fp * 60.0_fp), ap = 0.5_fp, as = 5.0_fp
 
+#elif defined (SKNET)
+
+  !!SK-net region
+  !!SK-net long-period ground motion (5-10s)
+  real(kind = fp), public, parameter :: eps = 1.0e-6_fp
+  real(kind = fp), public, parameter :: x_start = -250.0_fp, y_start = -250.0_fp, &
+  &                                     x_end   =  205.0_fp, y_end   =  225.0_fp
+  real(kind = fp), public, parameter :: center_lon = 139.0_fp, center_lat = 36.0_fp
+  real(kind = fp), public, parameter :: dgrid_x = 2.5_fp, dgrid_y = 2.5_fp
+  !!For 0.01s-sampled data  (SK-net)
+  integer,         public, parameter :: ntime = 18000
+  integer,         public, parameter :: ntimestep = 100
+  integer,         public, parameter :: ngradient2 = 1500
+  integer,         public, parameter :: ntime_slowness = 60
+  !!data order, array configuration
+  real(kind = fp), public, parameter :: order = 1.0e-6_fp !!nm -> mm
+  integer,         public, parameter :: nsta_grid_min = 10, nsta_grid_max = 4
+  integer,         public, parameter :: naddstation_array = 1
+  real(kind = fp), public, parameter :: cutoff_dist = 20.0_fp
+  real(kind = fp), public, parameter :: az_diff_max = 150.0_fp * deg2rad
+  !!Bandpass filter
+  real(kind = fp), public, parameter :: fl = 1.0_fp / 10.0_fp, fh = 1.0_fp / 5.0_fp, fs = fh * 2.0_fp, &
+  &                                     ap = 0.5_fp, as = 10.0_fp
+
 #else
 
+  !!S-net region
+  real(kind = fp), public, parameter :: x_start = -350.0_fp, y_start = -600.0_fp, &
+  &                                     x_end   = 350.0_fp,  y_end   = 600.0_fp
+  real(kind = fp), public, parameter :: center_lon = 142.5_fp, center_lat = 38.25_fp 
+  real(kind = fp), public, parameter :: dgrid_x = 20.0_fp, dgrid_y = 20.0_fp
+  !!DONET region
+  !real(kind = fp), public, parameter :: x_start = -150.0_fp, y_start = -100.0_fp, &
+  !&                                     x_end   = 150.0_fp,  y_end   = 100.0_fp
+  !real(kind = fp), public, parameter :: center_lon = 135.75_fp, center_lat = 33.2_fp 
+  !real(kind = fp), public, parameter :: dgrid_x = 10.0_fp, dgrid_y = 10.0_fp
   !!data order, array configuration
+  !!For 15s-sampled data 
+  integer,         public, parameter :: ntime = 2400
+  integer,         public, parameter :: ntimestep = 4 
+  integer,         public, parameter :: ngradient2 = 240
+  integer,         public, parameter :: ntime_slowness = 241
+  !!For 6s-sampled data
+  !integer,         public, parameter :: ntime = 6000
+  !integer,         public, parameter :: ntimestep = 10
+  !integer,         public, parameter :: ngradient2 = 600
+  !integer,         public, parameter :: ntime_slowness = 601, ntime_slowness2 = (ntime_slowness - 1) / 2
   !real(kind = fp), public, parameter :: order = 1.0e-2_fp !!Pa -> hPa
   real(kind = fp), public, parameter :: order = 1.0_fp 
   integer,         public, parameter :: nsta_grid_min = 4, nsta_grid_max = 4
@@ -81,16 +104,6 @@ module gradiometry_parameters
 
 #endif
 
-  !!SK-net region
-  !!data order, array configuration
-  !real(kind = fp), public, parameter :: order = 1.0e-6_fp !!nm -> mm
-  !integer,         public, parameter :: nsta_grid_min = 10, nsta_grid_max = 4
-  !integer,         public, parameter :: naddstation_array = 1
-  !real(kind = fp), public, parameter :: cutoff_dist = 20.0_fp
-  !real(kind = fp), public, parameter :: az_diff_max = 150.0_fp * deg2rad
-  !!Bandpass filter
-  !real(kind = fp), public, parameter :: fl = 1.0_fp / 10.0_fp, fh = 1.0_fp / 5.0_fp, fs = fh * 2.0_fp, &
-  !&                                     ap = 0.5_fp, as = 10.0_fp
 
   real(kind = fp), public, parameter :: error_omega = 2.0_fp * pi * 1.0_fp / ((1.0_fp / fl + 1.0_fp / fh) * 0.5_fp)
 
@@ -114,11 +127,5 @@ module gradiometry_parameters
 
   real(kind = fp), public, parameter :: grav_acc = 980.655_fp !!in cm/s^2
 
-  !!for AELUMA method
-  integer,         public, parameter :: ntime_fft = 1024, ntime_fft2 = ntime_fft / 2
-  real(kind = fp), public, parameter :: xcorr_min = 0.6_fp
-  real(kind = fp), public, parameter :: cos_taper_ratio = 0.1_fp
-  real(kind = fp), public, parameter :: lagtime_max = 20.0_fp * 60.0_fp, lagtime_min = -lagtime_max
- 
 end module gradiometry_parameters
   

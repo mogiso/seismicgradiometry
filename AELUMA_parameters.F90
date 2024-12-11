635deaca7cd1f9e2eb27fa39b857fac2f8d8d7e4
Masashi Ogiso <mogiso@mri-jma.go.jp>
2024-12-11 20:22:44 +0900

    initial commit

---
+++ AELUMA_parameters.F90
@@ -0,0 +1,33 @@
+!! Parameters of AELUMA method
+!! Copyright 2022 Masashi Ogiso (masashi.ogiso@gmail.com)
+!! Released under the MIT license.
+!! see https://opensource.org/licenses/MIT
+
+module aeluma_parameters
+  use nrtype, only : fp
+  implicit none
+  private
+
+  real(kind = fp), public, parameter :: eps = 1.0e-10_fp
+
+  
+  !!F-net aeluma method, read waveforms from shmdump
+  !!temporal origin: land-area centroid
+  real(kind = fp), public, parameter :: center_lon_aeluma = 137.6336_fp, center_lat_aeluma = 37.4581_fp
+  integer,         public, parameter :: nadd_station_aeluma = 1
+  !!Bandpass filter
+  real(kind = fp), public, parameter :: fl = 1.0_fp / 50.0_fp, fh = 1.0_fp / 2.0_fp, fs = fh * 2.0_fp, &
+  &                                     ap = 0.5_fp, as = 10.0_fp
+
+  integer,         public, parameter :: ntime = 2400
+  integer,         public, parameter :: ntimestep = 4   
+
+  real(kind = fp), public, parameter :: interstationdistance_min = 0.05_fp
+  real(kind = fp), public, parameter :: cutoff_dist = 100.0_fp
+  integer,         public, parameter :: ntime_fft = 1024, ntime_fft2 = ntime_fft / 2
+  real(kind = fp), public, parameter :: xcorr_min = 0.6_fp
+  real(kind = fp), public, parameter :: cos_taper_ratio = 0.1_fp
+  real(kind = fp), public, parameter :: lagtime_max = 20.0_fp * 60.0_fp, lagtime_min = -lagtime_max
+ 
+end module aeluma_parameters
+  
---
+++ AELUMA_shmdump.F90
@@ -0,0 +1,140 @@
+program AELUMA_shmdump
+  use nrtype, only : fp, sp
+  use tandem
+  use typedef
+  use calc_kernelmatrix, only : calc_slowness_est_matrix_delaunay_shmdump
+  use aeluma_parameters
+  use lonlat_xy_conv
+
+  implicit none
+
+  integer, parameter :: nwinch = 65536
+  integer, parameter :: nsampling_int = 2
+  integer, parameter :: sampling_int(1 : nsampling_int) = [100, 20]
+  integer, parameter :: sampling_int_use = 20
+  real(kind = fp), parameter :: sampling_sec(1 : nsampling_int) = 1.0_fp / real(sampling_int(1 : nsampling_int), kind = fp)
+  integer, parameter :: nsec_buf = 100
+  integer, parameter :: waveform_use_index_max = nsec_buf * sampling_int_use
+
+  character(len = 4) :: winch_char, comp_tmp
+  integer            :: i, j, ios, nsample, nch, nstation, winch_tmp, ndecimate, recflag, transdelay, mon_order, ad_bit, &
+  &                     sensor_amp, ntriangle
+  integer            :: yr(1 : nsec_buf), mo(1 : nsec_buf), dy(1 : nsec_buf), &
+  &                     hh(1 : nsec_buf), mm(1 : nsec_buf), ss(1 : nsec_buf), &
+  &                     waveform_tmp (1 : maxval(sampling_int))
+  integer, allocatable :: station_winch(:)
+  real(kind = fp)    :: waveform_real(1 : maxval(sampling_int)), waveform_use(1 : waveform_use_index_max, 1 : nwinch)
+  real(kind = fp)    :: station_sensitivity(1 : nwinch)
+  logical            :: is_usewinch(1 : nwinch)
+  character(len = 6) :: stname(1 : nwinch), stname_tmp
+  character(len = 255) :: chtbl, chtbl_line, sensor_unit
+  real(kind = fp)    :: ad_v_min, sensor_sens, naturalfreq, damp, &
+  &                     stlat_tmp, stlon_tmp, stelev_tmp, ptime_cor, stime_cor 
+  real(kind = fp), allocatable :: slowness_matrix(:, :, :)
+  integer, allocatable :: triangle_stationindex(:, :), nsta_count(:), tnbr(:, :)
+
+  type(location)     :: location_sta(1 : nwinch)
+  type(location), allocatable :: location_array(:), triangle_center(:)
+  !!band-pass filter
+  integer,         parameter :: filter_mode = 1
+  real(kind = fp), allocatable :: h(:, :), uv(:, :)
+  real(kind = fp)              :: gn(1 : nsampling_int), c(1 : nsampling_int)
+  integer                      :: m(1 : nsampling_int), n(1 : nsampling_int)
+
+  call getarg(1, chtbl)
+
+  !!filter design
+  do i = 1, nsampling_int
+    call calc_bpf_order(fl, fh, fs, ap, as, sampling_sec(i), m(i), n(i), c(i))
+  enddo
+  allocate(h(1 : 4 * maxval(m), 1 : nsampling_int), uv(1 : 4 * maxval(m), 1 : nwinch))
+  uv(1 : 4 * maxval(m), 1 : nwinch) = 0.0_fp
+  do i = 1, nsampling_int
+    call calc_bpf_coef(fl, fh, sampling_sec(i), m(i), n(i), h(:, i), c(i), gn(i))
+  enddo
+
+  !!read win-formatted channel table
+  is_usewinch(1 : nwinch) = .false.
+  open(unit = 10, file = trim(chtbl))
+  nstation = 0
+  do 
+    read(10, '(a256)', iostat = ios) chtbl_line
+    if(ios .ne. 0) exit
+    if(chtbl_line(1 : 1) .eq. "#") cycle
+    nstation = nstation + 1
+  enddo
+  rewind(10)
+  allocate(station_winch(1 : nstation))
+  i = 1
+  do
+    read(10, '(a255)', iostat = ios) chtbl_line
+    if(ios .ne. 0) exit
+    if(chtbl_line(1 : 1) .eq. "#") cycle
+    read(chtbl_line, *) winch_char, recflag, transdelay, stname_tmp, comp_tmp, mon_order, &
+    &                   ad_bit, sensor_sens, sensor_unit, naturalfreq, damp, sensor_amp, ad_v_min, &
+    &                   stlat_tmp, stlon_tmp, stelev_tmp, ptime_cor, stime_cor
+    read(winch_char, '(z4)') station_winch(i)
+    is_usewinch(station_winch(i)) = .true.
+    stname(station_winch(i)) = trim(stname_tmp)
+    station_sensitivity(station_winch(i)) = ad_v_min / (sensor_sens * 10.0_fp ** (sensor_amp / 20))
+    location_sta(station_winch(i))%lat = stlat_tmp
+    location_sta(station_winch(i))%lon = stlon_tmp
+    i = i + 1
+  enddo
+  close(10)
+  
+  !!set up arrays
+  !!convert (lon, lat) to (x, y)
+  do i = 1, nstation
+    call bl2xy(location_sta(station_winch(i))%lon, location_sta(station_winch(i))%lat, center_lon_aeluma, center_lat_aeluma, &
+    &          location_sta(station_winch(i))%y_north, location_sta(station_winch(i))%x_east)
+    location_sta(station_winch(i))%y_north = location_sta(station_winch(i))%y_north / 1000.0_fp
+    location_sta(station_winch(i))%x_east  = location_sta(station_winch(i))%x_east  / 1000.0_fp
+  enddo
+  !!calculate triads
+  call calc_slowness_est_matrix_delaunay_shmdump(location_sta, station_winch, &
+  &                                              ntriangle, triangle_center, slowness_matrix, triangle_stationindex, &
+  &                                              nsta_count, tnbr)
+  
+  
+
+  !!read waveforms from standard input, then conduct analysis
+  do
+    !!move previous datum
+    waveform_use(1 : waveform_use_index_max - sampling_int_use, 1 : nwinch) &
+    &  = waveform_use(sampling_int_use + 1 : waveform_use_index_max, 1 : nwinch)
+    yr(1 : nsec_buf - 1) = yr(2 : nsec_buf)
+    mo(1 : nsec_buf - 1) = mo(2 : nsec_buf)
+    dy(1 : nsec_buf - 1) = dy(2 : nsec_buf)
+    hh(1 : nsec_buf - 1) = hh(2 : nsec_buf)
+    mm(1 : nsec_buf - 1) = mm(2 : nsec_buf)
+    ss(1 : nsec_buf - 1) = ss(2 : nsec_buf)
+
+    !!read date from stdin
+    read(*, *, iostat = ios) yr(nsec_buf), mo(nsec_buf), dy(nsec_buf), hh(nsec_buf), mm(nsec_buf), ss(nsec_buf), nch
+    if(ios .ne. 0) error stop
+    write(0, '(a, 6(i2.2, 1x))') "reading ", &
+    &                             yr(nsec_buf), mo(nsec_buf), dy(nsec_buf), hh(nsec_buf), mm(nsec_buf), ss(nsec_buf)
+
+    !!read waveforms from stdin, filtering, 
+    do j = 1, nch
+      read(*, *) winch_char, nsample, (waveform_tmp(i), i = 1, nsample)
+      read(winch_char, '(z4)') winch_tmp
+      waveform_real(1 : nsample) = real(waveform_tmp(1 : nsample), kind = fp) * station_sensitivity(winch_tmp)
+      do i = 1, nsampling_int
+        if(nsample .eq. sampling_int(i)) exit
+      enddo
+      call tandem3(waveform_real(1 : nsample), h(:, i), gn(i), filter_mode, past_uv=uv(:, winch_tmp))
+      ndecimate = sampling_int(i) / sampling_int_use
+      do i = 1, nsample / ndecimate
+        waveform_use(waveform_use_index_max - sampling_int_use + i, winch_tmp) = waveform_real(ndecimate * (i - 1) + 1)
+      enddo
+    enddo
+
+
+
+  enddo
+
+  stop
+end program AELUMA_shmdump
+

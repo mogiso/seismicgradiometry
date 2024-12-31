72bac09795bcaa877bd33d6802b74e7dbe9475e7
Masashi Ogiso <mogiso@mri-jma.go.jp>
2024-12-31 21:26:51 +0900

    use min_correlation as a weight

--- plot_map_vector.F90
+++ plot_map_vector.F90
@@ -10,10 +10,11 @@ program plot_map_vector
   real(kind = fp), parameter :: lon_w = 120.0_fp, lon_e = 149.0_fp, lat_s = 22.5_fp, lat_n = 48.0_fp, center_lon = 135.0_fp
   integer, parameter :: iwin = 0, iwin_legend = 1
 
-  integer         :: i, j, k, ios, ncoastline, mapcount, narray, arrayindex, ntriangle, color(1 : 3), max_similarity(1)
+  integer         :: i, j, k, ios, ncoastline, mapcount, narray, ntriangle, color(1 : 3), max_similarity(1)
   real(kind = fp) :: width_min, width_max, height_min, height_max, dwidth, dheight, maplon, maplat, map_x, map_y, &
   &                  map_x1, map_y1
   real(kind = fp), allocatable :: slowness_x(:), slowness_y(:), lon_array(:), lat_array(:), min_correlation(:)
+  integer,         allocatable :: arrayindex(:)
   logical, allocatable :: result_exist(:)
   real(kind = sp) :: plot_x, plot_y, plot_x1, plot_y1, plot_theta
   character(len = 255) :: coastline_txt, mapbuf_tmp
@@ -23,16 +24,18 @@ program plot_map_vector
   character(len = 6)  :: plottext
   
   !!location estimation
-  integer, parameter   :: nparticle = 1000, niter = 10
+  integer, parameter   :: nparticle = 1000, niter = 6
   real(kind = fp), parameter :: daz_weight = 10.0_fp * deg2rad
   real(kind = fp), parameter :: sigma_particle = 0.1_fp
   real(kind = fp), parameter :: cos_similarity_accept_degree = 5.0_fp * deg2rad
+  real(kind = fp), parameter :: correlation_threshold = 0.5_fp
   integer, allocatable :: seed(:)
   integer              :: seedsize
   real(kind = fp)      :: cos_similarity(1 : nparticle), lon_particle(1 : nparticle), lat_particle(1 : nparticle), &
   &                       lon_particle_new(1 : nparticle), lat_particle_new(1 : nparticle), particle_probability(1 : nparticle), &
   &                       az_weight(1 : int(2.0_fp * pi / daz_weight))
-  real(kind = fp)      :: rnd, rnd1, rnd2, az, normalize_cos_similarity, cos_similarity_tmp
+  real(kind = fp)      :: rnd, rnd1, rnd2, normalize_cos_similarity, cos_similarity_tmp
+  real(kind = fp), allocatable :: az(:)
 
   call random_seed(size = seedsize)
   allocate(seed(1 : seedsize))
@@ -149,8 +152,8 @@ program plot_map_vector
     read(*, *, iostat = ios) yr, mo, dy, hh, mm, ss, narray, ntriangle
     if(ios .ne. 0) stop
     if(.not. allocated(slowness_x)) then
-      allocate(slowness_x(1 : ntriangle), slowness_y(1 : ntriangle), result_exist(1 : ntriangle), &
-      &        lon_array(1 : ntriangle), lat_array(1 : ntriangle), min_correlation(1 : ntriangle))
+      allocate(slowness_x(1 : ntriangle), slowness_y(1 : ntriangle), result_exist(1 : ntriangle), arrayindex(1 : ntriangle), &
+      &        lon_array(1 : ntriangle), lat_array(1 : ntriangle), min_correlation(1 : ntriangle), az(1 : ntriangle))
     endif
     date_txt = "20" // yr // "/" // mo // "/" // dy // " " // hh // ":" // mm // ":" // ss
     call mercator(center_lon, lon_w, lat_n, map_x, map_y)
@@ -189,24 +192,24 @@ program plot_map_vector
       result_exist(1 : ntriangle) = .false.
       !!read and plot slowness vector
       do i = 1, narray
-        read(*, *) arrayindex, lon_array(arrayindex), lat_array(arrayindex), &
-        &          slowness_x(arrayindex), slowness_y(arrayindex), min_correlation(arrayindex)
-        if(.not. (slowness_x(arrayindex) .ne. 0.0_fp .and. slowness_y(arrayindex) .ne. 0.0_fp)) cycle
-        result_exist(arrayindex) = .true.
+        read(*, *) arrayindex(i), lon_array(arrayindex(i)), lat_array(arrayindex(i)), &
+        &          slowness_x(arrayindex(i)), slowness_y(arrayindex(i)), min_correlation(arrayindex(i))
+        if(.not. (slowness_x(arrayindex(i)) .ne. 0.0_fp .and. slowness_y(arrayindex(i)) .ne. 0.0_fp)) cycle
+        result_exist(arrayindex(i)) = .true.
         !theta = atan2(slowness_x, slowness_y) * rad2deg
-        plot_theta = real(atan2(slowness_y(arrayindex), slowness_x(arrayindex)) * rad2deg, kind = sp)
-        call mercator(center_lon, lon_array(arrayindex), lat_array(arrayindex), map_x, map_y)
+        plot_theta = real(atan2(slowness_y(arrayindex(i)), slowness_x(arrayindex(i))) * rad2deg, kind = sp)
+        call mercator(center_lon, lon_array(arrayindex(i)), lat_array(arrayindex(i)), map_x, map_y)
         plot_x  = real((map_x  - width_min)  * dwidth,  kind = sp) * width
         plot_y  = real((map_y  - height_min) * dheight, kind = sp) * height
-        if(min_correlation(arrayindex) .lt. 0.2_fp) then
+        if(min_correlation(arrayindex(i)) .lt. 0.2_fp) then
           color(1 : 3) = [220, 204, 222]
-        elseif(min_correlation(arrayindex) .ge. 0.2_fp .and. min_correlation(arrayindex) .lt. 0.4_fp) then
+        elseif(min_correlation(arrayindex(i)) .ge. 0.2_fp .and. min_correlation(arrayindex(i)) .lt. 0.4_fp) then
           color(1 : 3) = [212, 156, 189]
-        elseif(min_correlation(arrayindex) .ge. 0.4_fp .and. min_correlation(arrayindex) .lt. 0.6_fp) then
+        elseif(min_correlation(arrayindex(i)) .ge. 0.4_fp .and. min_correlation(arrayindex(i)) .lt. 0.6_fp) then
           color(1 : 3) = [196, 110, 155]
-        elseif(min_correlation(arrayindex) .ge. 0.6_fp .and. min_correlation(arrayindex) .lt. 0.8_fp) then
+        elseif(min_correlation(arrayindex(i)) .ge. 0.6_fp .and. min_correlation(arrayindex(i)) .lt. 0.8_fp) then
           color(1 : 3) = [136, 97, 141]
-        elseif(min_correlation(arrayindex) .ge. 0.8_fp) then
+        elseif(min_correlation(arrayindex(i)) .ge. 0.8_fp) then
           color(1 : 3) = [73, 57, 100]
         endif
         call pc_setcolor(iwin, color(1), color(2), color(3))
@@ -223,44 +226,51 @@ program plot_map_vector
           call random_number(rnd)
           lat_particle(i) = lat_s + (lat_n - lat_s) * rnd
         enddo
-        do k = 1, niter
+        particlefilter: do k = 1, niter
           !!calculate cosine-similarity
           do j = 1, nparticle
             cos_similarity(j) = 0.0_fp
             particle_probability(j) = 0.0_fp
             az_weight(1 : int(2.0_fp * pi / daz_weight)) = 0.0_fp
-            do i = 1, ntriangle
-              if(result_exist(i) .eqv. .false.) cycle
-              call greatcircle_dist(lat_array(i), lon_array(i), lat_particle(j), lon_particle(j), azimuth = az)
-              az = az + pi
-              if(az .gt. 2.0_fp * pi) az = az - 2.0_fp * pi
-              az_weight(int(az / daz_weight) + 1) = az_weight(int(az / daz_weight) + 1) + 1.0_fp
+            do i = 1, narray
+              if(min_correlation(i) .lt. correlation_threshold) cycle
+              call greatcircle_dist(lat_array(arrayindex(i)), lon_array(arrayindex(i)), &
+              &                     lat_particle(j), lon_particle(j), azimuth = az(i))
+              az(i) = az(i) + pi
+              if(az(i) .ge. 2.0_fp * pi) az(i) = az(i) - 2.0_fp * pi
+              az_weight(int(az(i) / daz_weight) + 1) = az_weight(int(az(i) / daz_weight) + 1) + 1.0_fp
             enddo
-            do i = 1, ntriangle
-              if(result_exist(i) .eqv. .false.) cycle
-              call greatcircle_dist(lat_array(i), lon_array(i), lat_particle(j), lon_particle(j), azimuth = az)
-              az = az + pi
-              if(az .gt. 2.0_fp * pi) az = az - 2.0_fp * pi
-              cos_similarity_tmp =(slowness_x(i) * sin(az) + slowness_y(i) * cos(az)) &
-              &                  / sqrt(slowness_x(i) ** 2 + slowness_y(i) ** 2) 
+            do i = 1, narray
+              if(min_correlation(i) .lt. correlation_threshold) cycle
+              cos_similarity_tmp = (slowness_x(arrayindex(i)) * sin(az(i)) + slowness_y(arrayindex(i)) * cos(az(i))) &
+              &              / sqrt(slowness_x(arrayindex(i)) ** 2 + slowness_y(arrayindex(i)) ** 2)
               if(cos_similarity_tmp .le. cos(cos_similarity_accept_degree)) cos_similarity_tmp = 0.0_fp
-              cos_similarity(j) = cos_similarity(j) + cos_similarity_tmp / az_weight(int(az / daz_weight) + 1)
+              cos_similarity(j) = cos_similarity(j) &
+              &                 + cos_similarity_tmp / az_weight(int(az(i) / daz_weight) + 1) * min_correlation(arrayindex(i))
             enddo
           enddo
           if(sum(cos_similarity) .eq. 0.0_fp) exit
-          normalize_cos_similarity = 1.0_fp / sum(cos_similarity)
+          normalize_cos_similarity = 1.0_fp / sum(cos_similarity(1 : nparticle))
+          if(k .eq. niter) exit particlefilter
           particle_probability(1) = cos_similarity(1) * normalize_cos_similarity
           do i = 2, nparticle
             particle_probability(i) = particle_probability(i - 1) + cos_similarity(i) * normalize_cos_similarity
           enddo
           !!check
           !write(0, *) "max_particle_prob = ", particle_probability(nparticle)
+
           !!redistribute particle
           do j = 1, nparticle
             call random_number(rnd)
             do i = 1, nparticle
               if(rnd .le. particle_probability(i)) exit
             enddo
+            if(i .gt. nparticle) then
+              write(0, *) i, rnd, particle_probability(i - 1), normalize_cos_similarity
+              do i = 1, narray
+                write(0, *) az(i), min_correlation(arrayindex(i))
+              enddo
+            endif
             call random_number(rnd1)
             call random_number(rnd2)
             rnd = sqrt(-2.0_fp * log(rnd1)) * cos(2.0_fp * pi * rnd2)
@@ -272,7 +282,7 @@ program plot_map_vector
           enddo
           lon_particle(1 : nparticle) = lon_particle_new(1 : nparticle)
           lat_particle(1 : nparticle) = lat_particle_new(1 : nparticle)
-        enddo
+        enddo particlefilter
          
         !!write circles
         call pc_setline(iwin, 1)

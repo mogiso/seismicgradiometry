0564f3aec6c2d31d2fd1b01568a88b7b3a6bfab1
Masashi Ogiso <mogiso@mri-jma.go.jp>
2025-02-21 18:36:42 +0900

    commit temporally

--- typedef.F90
+++ typedef.F90
@@ -7,4 +7,10 @@ module typedef
     real(kind = fp) :: lon, lat, x_east, y_north, depth
   end type location
 
+  type trigger_list
+    real(kind = fp) :: lon_array, lat_array, az_obs, appvel_obs, min_correlation, arrivaltime
+    integer         :: ref_yr, ref_jday, ref_secfromday
+    logical         :: trig_exist, trig_used
+  end type trigger_list
+
 end module typedef

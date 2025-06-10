4eec8be06d0d8ab2a4296fd558df0b00377679fd
Masashi Ogiso <mogiso@mri-jma.go.jp>
2025-06-10 15:56:54 +0900

    plot likelihood in addition to slowness

--- plot_slowness_likelihood_gmt.pl
+++ plot_slowness_likelihood_gmt.pl
@@ -11,17 +11,33 @@ $lat_s = 23;
 $lat_n = 47;
 $annot_lon = "a5";
 $annot_lat = "a5";
+
 $appvelcpt = "appvel.cpt";
+$annot_appvelcpt = "+l\"Apparent velocity (km/s)\"";
+$appvelcpt_y = -1.0;
 $vector_len = "0.5c";
+$vector_shape = "0.12/0.23/0.2";
+
+$likelihoodcpt = "likelihood.cpt";
+$annot_likelihoodcpt = "+l\"Normalized likelihood\"";
+$likelihoodcpt_y = -3.0;
+$particle_size = 0.2;
+$symbolpen = "0.4p,black";
+
+$cpt_x = $size_x / 2.0;
+$cpt_len = $size_x;
+$cpt_width = "0.3ch";
+
 
 system "gmt set PS_LINE_JOIN round";
+system "gmt set LABEL_FONT 12p,Helvetica,black";
 
-system "cat /dev/null | gmt psxy -JX1c -R0/1/0/1 -Sc0.1 -K -P -X3c -Y4c > $out";
+system "cat /dev/null | gmt psxy -JX1c -R0/1/0/1 -Sc0.1 -K -P -X3c -Y7c > $out";
 
-system "gmt pscoast -JM$size_x -R$lon_w/$lon_e/$lat_s/$lat_n -W0.8p,black -Dh \\
-                    -Bpx$annot_lon -Bpy$annot_lat -BWSen -O -K -P >> $out";
+system "gmt psxy $in_likelihood -bif -JM$size_x -R$lon_w/$lon_e/$lat_s/$lat_n \\
+                                -Sc$particle_size -C$likelihoodcpt -W$symbolpen -O -K -P >> $out";
 
-open OUT, " | gmt psxy -JM$size_x -R$lon_w/$lon_e/$lat_s/$lat_n -SVb0.15/0.3/0.2 -C$appvelcpt -W0.8p,black -O -K -P >> $out";
+open OUT, " | gmt psxy -JM$size_x -R$lon_w/$lon_e/$lat_s/$lat_n -SVb$vector_shape -C$appvelcpt -W$symbolpen -O -K -P >> $out";
 $filesize = -s $in_vector;
 $fileread = 0;
 open IN, "<", $in_vector;
@@ -40,6 +56,11 @@ while($fileread < $filesize){
 close IN;
 close OUT;
 
+system "gmt pscoast -JM$size_x -R$lon_w/$lon_e/$lat_s/$lat_n -W0.5p,black -Di \\
+                    -Bpx$annot_lon -Bpy$annot_lat -BWSen -O -K -P >> $out";
+
+system "gmt psscale -Dx$cpt_x/$appvelcpt_y/$cpt_len/$cpt_width -C$appvelcpt -B$annot_appvelcpt -O -K -P >> $out";
+system "gmt psscale -Dx$cpt_x/$likelihoodcpt_y/$cpt_len/$cpt_width -C$likelihoodcpt -B$annot_likelihoodcpt -Q -O -K -P >> $out";
 
 system "cat /dev/null | gmt psxy -JX1c -R0/1/0/1 -Sc0.1 -O -P >> $out";
 

e4139d362d1b03e2afcf94064f75b7d721cd2570
Masashi Ogiso <mogiso@mri-jma.go.jp>
2025-06-10 15:55:43 +0900

    modify correlation_threshold

--- AELUMA_parameters.F90
+++ AELUMA_parameters.F90
@@ -78,10 +78,10 @@ module aeluma_parameters
   real(kind = fp),    public, parameter :: sigma_particle = 0.3_fp
   real(kind = fp),    public, parameter :: sigma_dist = 500.0_fp, ot_coef = 0.5_fp, sigma_otdiff = 60.0_fp, &
   &                                        sigma_dist2 = sigma_dist ** 2, sigma_otdiff2 = sigma_otdiff ** 2
-  real(kind = fp),    public, parameter :: correlation_threshold = 0.5_fp
+  real(kind = fp),    public, parameter :: correlation_threshold = 0.45_fp
   real(kind = fp),    public, parameter :: min_appvelocity = 0.3_fp
 
-  integer,            public, parameter :: narray_use_min = 6 
+  integer,            public, parameter :: narray_use_min = 6
   integer,            public, parameter :: epicenter_acceptcount_threshold = 60
   real(kind = fp),    public, parameter :: min_likelihood_eqobs = 0.5_fp / (pi * daz_weight * sigma_otdiff) &
   &                                                             * exp(-0.5_fp * (3.0_fp ** 2))

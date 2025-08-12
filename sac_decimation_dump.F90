242829b6c4d4dffa7f54516ab105c6f0d38218ad
Masashi Ogiso <mogiso@mri-jma.go.jp>
2025-08-12 18:46:04 +0900

    initial commit

---
+++ sac_decimation_dump.F90
@@ -0,0 +1,75 @@
+program sac_decimation_dump
+  use nrtype, only : fp
+  use constants, only : pi
+  use read_sacfile, only : read_sachdr, read_sacdata
+  use tandem, only : tandem3
+
+  implicit none
+
+  real(kind = fp), parameter :: ap = 0.1_fp, as = 15.0_fp
+  integer, parameter :: nmean = 100 * 15
+  
+  integer :: i, j, nfile, npts, decimate, jday, yr, mo, dy, hh, mm
+  character(len = 4) :: sachdr_char(1 : 158)
+  character(len = 129), allocatable :: sacfile(:)
+  character(len = 129) :: outfile
+  real(kind = fp), allocatable :: waveform_org(:)
+  real(kind = fp) :: mean, sampling, sampling_new, waveform_decimate, ss
+
+  real(kind = fp), allocatable :: h(:)
+  real(kind = fp) :: c, gn, fl, fh, fs, begintime, sec_from_day
+  integer :: m, n 
+  character(len = 65) :: decimate_c, fl_c, fh_c, fs_c
+
+  call getarg(1, decimate_c); read(decimate_c, *) decimate
+  call getarg(2, fl_c); read(fl_c, *) fl
+  call getarg(3, fh_c); read(fh_c, *) fh
+  call getarg(4, fs_c); read(fs_c, *) fs
+  call getarg(5, sacfile)
+
+  open(unit = 10, file = trim(sacfile), form = "unformatted", access = "direct", recl = 4)
+
+  call read_sachdr(trim(sacfile), delta = sampling, npts = npts, begin = begintime, year = yr, julianday = jday)
+  allocate(waveform_org(1 : npts))
+  call read_sacdata(trim(sacfile), npts, waveform_org)
+
+  mean = 0.0_fp
+  do i = 1, nmean
+    mean = mean + waveform_org(i)
+  enddo
+  mean = mean / real(nmean, kind = fp)
+  waveform_org(1 : npts) = waveform_org(1 : npts) - mean
+
+  write(0, '(a)') "Appling band-pass filter"
+  !!new sampling period
+  sampling_new = sampling * real(decimate, kind = fp)
+  !!fp and fs are determined from new sampling period
+  write(0, '(a, 2(f6.3, 1x))') "old and new sampling period = ", sampling, sampling_new
+  write(0, '(a, e15.7)') "new nyquist frequency (Hz) = ", 1.0_fp / (sampling_new * 2.0_fp)
+  write(0, '(a, 3(e15.7, 1x))') "parameter fl, fh, fs (Hz) = ", fl, fh, fs
+  call calc_bpf_order(fl, fh, fs, ap, as, sampling, m, n, c)
+  allocate(h(4 * m))
+  call calc_bpf_coef(fl, fpass, sampling, m, n, h, c, gn)
+  call tandem3(waveform_org, h, gn, 1)
+  !call tandem3(waveform_org, h, gn, -1)
+
+  do i = 1, npts / decimate
+    waveform_decimate = waveform_org(decimate * (i - 1) + 1)
+    sec_from_day = begin + sampling * real(decimate * (i - 1), kind = fp)
+    if(sec_from_day >= 86400.0_fp) then
+      jday = jday + 1
+      sec_from_day = sec_from_day - 86400.0_fp
+    endif
+    call jday2ymd(jday, yr, mo, dy)
+    hh = int(sec_from_day / (60.0_fp * 60.0_fp))
+    mm = int((sec_from_day - 60.0_fp * 60.0_fp * real(hh, kind = fp)) / 60.0_fp)
+    ss = sec_from_day - 60.0_fp * 60.0_fp * real(hh, kind = fp) - 60.0_fp * real(mm, kind = fp)
+    print *, yr, mo, dy, hh, mm, ss, waveform_decimate
+  enddo
+
+
+  deallocate(h, waveform_org)
+
+  stop
+end program sac_decimation_dump
+    

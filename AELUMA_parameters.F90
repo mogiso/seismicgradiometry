02568a8bde198208e373f8f01e189edd92924172
Masashi Ogiso <mogiso@mri-jma.go.jp>
2025-08-14 11:33:28 +0900

    modify nsta_use_min, correlation_threshold

--- AELUMA_parameters.F90
+++ AELUMA_parameters.F90
@@ -51,13 +51,13 @@ module aeluma_parameters
   real(kind = fp),    public, parameter :: sigma_particle = 0.3_fp
   real(kind = fp),    public, parameter :: sigma_dist = log(100.0_fp), ot_coef = 0.7_fp, sigma_otdiff = 40.0_fp, &
   &                                        sigma_dist2 = sigma_dist ** 2, sigma_otdiff2 = sigma_otdiff ** 2
-  real(kind = fp),    public, parameter :: correlation_threshold = 0.6_fp
+  real(kind = fp),    public, parameter :: correlation_threshold = 0.5_fp
   real(kind = fp),    public, parameter :: ref_appvelocity = 0.3_fp
   real(kind = fp),    public, parameter :: max_slowness = 0.4_fp
 
-  integer,            public, parameter :: narray_use_min = 5
+  integer,            public, parameter :: narray_use_min = 6
   integer,            public, parameter :: epicenter_acceptcount_threshold = 90
-  integer,            public, parameter :: epicenter_renew_threshold = 90
+  integer,            public, parameter :: epicenter_renew_threshold = 120
   real(kind = fp),    public, parameter :: min_likelihood_eqobs = 0.5_fp / (pi * sigma_azdiff * sigma_otdiff) &
   &                                                             * exp(-0.5_fp * (3.0_fp ** 2))
 

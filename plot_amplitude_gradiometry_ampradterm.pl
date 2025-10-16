d2356b59495d71092825ccdadc0e6c4be92188dd
Masashi Ogiso <mogiso@mri-jma.go.jp>
2025-10-16 20:32:49 +0900

    modify for Kamchatka Tsunami plot

--- plot_amplitude_gradiometry_ampradterm.pl
+++ plot_amplitude_gradiometry_ampradterm.pl
@@ -1,13 +1,14 @@
 #!/usr/bin/env perl
 use Math::Trig qw(pi rad2deg deg2rad);
 use Parallel::ForkManager;
-$MAX_PROCESS = 15;
+$MAX_PROCESS = 10;
 
 $in_dir = $ARGV[0];
 $index_begin = $ARGV[1];
 $index_end = $ARGV[2];
 $coastline = $ARGV[3];
 $simulation = $ARGV[4];
+$tidestation = $ARGV[5];
 
 $ref_yr = "2025";
 $ref_mo = "7";
@@ -21,6 +22,9 @@ $ref_sec = $ref_hh * 3600 + $ref_mm * 60 + $ref_ss;
 if($simulation == 1){
   $ref_sec = 0;
 }
+if($simulation == 0){
+  $ref_sec = -(86400 + 8 * 3600 + 24 * 60 + 52.0); 
+}
 
 #S-net ref: 142.5E, 38.25N
 $dgrid_x = 20;
@@ -29,7 +33,7 @@ $min_x = -350;
 $max_x = 350;
 $min_y = -600;
 $max_y = 600;
-$size_x = 4.8;
+$size_x = 3.8;
 $size_y = 8.22;
 $annot = "a200f100";
 
@@ -44,13 +48,17 @@ $annot = "a200f100";
 #$size_y = 3.2;
 #$annot = "a100f50";
 
-$dx = $size_x + 0.8;
+$dx = $size_x + 0.7;
 
 
 $ngrid_x = int(($max_x - $min_x) / $dgrid_x) + 1;
 $ngrid_y = int(($max_y - $min_y) / $dgrid_y) + 1;
 
+
 $cpt = "amplitude_gradiometry_OBP.cpt";
+if($simulation == 0){
+  $cpt = "amplitude_gradiometry_OBP.cpt";
+}
 if($simulation == 1){
   $cpt = "amplitude_gradiometry_OBP_simulation.cpt";
 }
@@ -60,11 +68,25 @@ if($simulation == 1){
 #$cpt = "amplitude_gradiometry_OBP_231202.cpt";
 $app_vel_cpt = "app_vel_gradiometry_OBP.cpt";
 #$ampterm_cpt = "ampterm_OBP.cpt";
-$ampterm_cpt = "ampterm_OBP_simulation.cpt";
+$ampterm_cpt = "ampterm_OBP_simulation2.cpt";
 $tsunami_vel_grd = "tsunami_velocity_etopo.grd";
 
-$txt_x = 0.15;
-$txt_y = $size_y - 0.15;
+if (-f $tidestation){
+  open IN, "<", $tidestation;
+  while(<IN>){
+    chomp $_;
+    $_ =~ s/^\s*(.*?)\s*$/$1/;
+    @tmp = split /\s+/, $_;
+    push @tide_lon, $tmp[0];
+    push @tide_lat, $tmp[1];
+    push @tide_xeast, $tmp[2];
+    push @tide_ynorth, $tmp[3];
+  }
+  close IN;
+}
+
+$txt_x = 0.1;
+$txt_y = $size_y - 0.1;
 $cpt_x = $size_x / 2.0;
 $cpt_y = -1.4;
 $cpt_len = $size_x;
@@ -72,12 +94,12 @@ $cpt_width = "0.3ch";
 $slowness_length = 0.35;
 $decimate = 2;
 
-$txt_x2 = -1.7;
+$txt_x2 = -1.4;
 $txt_x3 = -0.2;
 $txt_y2 = $size_y + 0.3;
 
 system "gmt set PS_LINE_JOIN round";
-system "gmt set FONT_LABEL 10p,Helvetica";
+system "gmt set FONT_LABEL 9p,Helvetica";
 system "gmt set FONT_ANNOT_PRIMARY 9p,Helvetica";
 system "gmt set MAP_LABEL_OFFSET 5p";
 system "gmt set GMT_AUTO_DOWNLOAD off";
@@ -97,7 +119,7 @@ foreach $index (@index_array){
   $time_index = sprintf "%04d", $index;
   $current_sec = $ref_sec + ($index - 1) * $dt;
 
-  if($simulation != 1){
+  if($simulation != 1 && $simulation != 0){
     $current_yr = $ref_yr;
     $current_mo = $ref_mo;
     $current_dy = $ref_dy;
@@ -126,31 +148,48 @@ foreach $index (@index_array){
   print stderr "$out\n";
 
   
-  system "cat /dev/null | gmt psxy -JX1c -R0/1/0/1 -Sc0.1 -K -X3c -Y6c > $out";
+  system "cat /dev/null | gmt psxy -JX1c -R0/1/0/1 -Sc0.1 -K -X2c -Y6c -P > $out";
 
   system "gmt grdimage $in -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -C$cpt -O -K >> $out";
   system "gmt psbasemap -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -Bpx${annot}+l\"Easting (km)\" \\
                         -Bpy${annot}+l\"Northing (km)\" -BWSen -O -K >> $out";
   if (-f $coastline){
-    system "gmt psxy $coastline -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -W0.8p,black -O -K >> $out";
+    system "gmt psxy $coastline -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -W0.4p,black -O -K >> $out";
   }
-  open OUT, " | gmt pstext -JX$size_x/$size_y -R0/$size_x/0/$size_y -N -F+f+a+j -Gwhite -O -K >> $out";
+  if (-f $tidestation){
+    open OUT, " | gmt psxy -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -Si0.25 -W0.5p,whitesmoke -Gblack -O -K >> $out";
+    for($i = 0; $i <= $#tide_xeast; $i++){
+      print OUT "$tide_xeast[$i] $tide_ynorth[$i]\n";
+    }
+    close OUT;
+  }
     if($simulation == 1){
-      print OUT "$txt_x $txt_y 12p,Helvetica,black 0 LT Time: ${current_hh}hr ${current_mm}m\n";
+      open OUT, " | gmt pstext -JX$size_x/$size_y -R0/$size_x/0/$size_y -M -N -F+f+a+j -Gwhite -O -K >> $out";
+        print OUT "> $txt_x $txt_y 9p,Helvetica,black 0 LT 10p 2.23c l\nTime: ${current_hh}hr ${current_mm}m\nSynthetic\n";
+      close OUT;
+    }elsif($simulation == 0){
+      open OUT, " | gmt pstext -JX$size_x/$size_y -R0/$size_x/0/$size_y -M -N -F+f+a+j -Gwhite -O -K >> $out";
+        print OUT "> $txt_x $txt_y 9p,Helvetica,black 0 LT 10p 2.46c l\nTime: ${current_hh}hr ${current_mm}m\nObservation\n";
+      close OUT;
     }else{
-      print OUT "$txt_x $txt_y 12p,Helvetica,black 0 LT $current_yr/$current_mo/$current_dy $current_hh:$current_mm\n";
+      open OUT, " | gmt pstext -JX$size_x/$size_y -R0/$size_x/0/$size_y -N -F+f+a+j -Gwhite -O -K >> $out";
+        print OUT "$txt_x $txt_y 9p,Helvetica,black 0 LT $current_yr/$current_mo/$current_dy $current_hh:$current_mm\n";
+      close OUT;
     } 
-    print OUT "$txt_x2 $txt_y2 14p,Helvetica,black 0 LB (a)\n";
+  open OUT, " | gmt pstext -JX$size_x/$size_y -R0/$size_x/0/$size_y -N -F+f+a+j -Gwhite -O -K >> $out";
+    print OUT "$txt_x2 $txt_y2 12p,Helvetica,black 0 LB (a)\n";
   close OUT;
 
   if (-f "$in_dir/station_location.txt"){
     system "gmt psxy $in_dir/station_location.txt -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -Sc0.1 -W0.5p,black -O -K >> $out";
   }
 
-  if($simulation != 1){
+  if($simulation == 0){
     system "gmt psscale -Dx$cpt_x/$cpt_y/$cpt_len/$cpt_width -B+l\"Amplitude (hPa)\" -C$cpt -O -K >> $out";
-  }else{
+  }elsif($simulation == 1){
     system "gmt psscale -Dx$cpt_x/$cpt_y/$cpt_len/$cpt_width -B+l\"Amplitude (m)\" -C$cpt -O -K >> $out";
+  }else{
+    system "gmt psscale -Dx$cpt_x/$cpt_y/$cpt_len/$cpt_width -B+l\"Amplitude (hPa)\" -C$cpt -O -K >> $out";
   }
 
   system "cat /dev/null | gmt psxy -JX1c -R0/1/0/1 -Sc0.1 -O -K -X$dx >> $out";
@@ -158,7 +197,7 @@ foreach $index (@index_array){
   system "gmt psbasemap -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -Bpx${annot}+l\"Easting (km)\" -Bpy${annot} \\
                         -BSwen -O -K >> $out";
   if (-f $coastline){
-    system "gmt psxy $coastline -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -W0.8p,black -O -K >> $out";
+    system "gmt psxy $coastline -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -W0.4p,black -O -K >> $out";
   }
 
   #if (-f "$in_dir/station_location.txt"){
@@ -237,6 +276,13 @@ foreach $index (@index_array){
     close OUT;
 
   }
+  if (-f $tidestation){
+    open OUT, " | gmt psxy -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -Si0.25 -W0.5p,whitesmoke -Gblack -O -K >> $out";
+    for($i = 0; $i <= $#tide_xeast; $i++){
+      print OUT "$tide_xeast[$i] $tide_ynorth[$i]\n";
+    }
+    close OUT;
+  }
 
   open OUT, " | gmt pstext -JX$size_x/$size_y -R0/$size_x/0/$size_y -N -F+f+a+j -O -K >> $out";
     print OUT "$txt_x3 $txt_y2 14p,Helvetica,black 0 LB (b)\n";
@@ -270,13 +316,20 @@ foreach $index (@index_array){
   #  system "gmt psxy $in_dir/station_location.txt -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -Sc0.1 -W0.5p,dimgray -O -K >> $out";
   #}
   if (-f $coastline){
-    system "gmt psxy $coastline -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -W0.8p,black -O -K >> $out";
+    system "gmt psxy $coastline -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -W0.4p,black -O -K >> $out";
+  }
+  if (-f $tidestation){
+    open OUT, " | gmt psxy -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -Si0.25 -W0.5p,whitesmoke -Gblack -O -K >> $out";
+    for($i = 0; $i <= $#tide_xeast; $i++){
+      print OUT "$tide_xeast[$i] $tide_ynorth[$i]\n";
+    }
+    close OUT;
   }
   open OUT, " | gmt pstext -JX$size_x/$size_y -R0/$size_x/0/$size_y -N -F+f+a+j -Gwhite -O -K >> $out";
     print OUT "$txt_x3 $txt_y2 14p,Helvetica,black 0 LB (c)\n";
   close OUT;
 
-  system "gmt psscale -Dx$cpt_x/$cpt_y/$cpt_len/$cpt_width -B+l\"Geometrical spreading (10\@+-2\@+ km\@+-1\@+)\" \\
+  system "gmt psscale -Dx$cpt_x/$cpt_y/$cpt_len/$cpt_width -Ba1f0.5g0.5+l\"Geometrical spreading (10\@+-2\@+ km\@+-1\@+)\" \\
                        -C$ampterm_cpt -O -K >> $out";
 
 
@@ -303,13 +356,20 @@ foreach $index (@index_array){
   #  system "gmt psxy $in_dir/station_location.txt -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -Sc0.1 -W0.5p,dimgray -O -K >> $out";
   #}
   if (-f $coastline){
-    system "gmt psxy $coastline -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -W0.8p,black -O -K >> $out";
+    system "gmt psxy $coastline -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -W0.4p,black -O -K >> $out";
+  }
+  if (-f $tidestation){
+    open OUT, " | gmt psxy -JX$size_x/$size_y -R$min_x/$max_x/$min_y/$max_y -Si0.25 -W0.5p,whitesmoke -Gblack -O -K >> $out";
+    for($i = 0; $i <= $#tide_xeast; $i++){
+      print OUT "$tide_xeast[$i] $tide_ynorth[$i]\n";
+    }
+    close OUT;
   }
   open OUT, " | gmt pstext -JX$size_x/$size_y -R0/$size_x/0/$size_y -N -F+f+a+j -Gwhite -O -K >> $out";
     print OUT "$txt_x3 $txt_y2 14p,Helvetica,black 0 LB (d)\n";
   close OUT;
 
-  system "gmt psscale -Dx$cpt_x/$cpt_y/$cpt_len/$cpt_width -B+l\"Radiation pattern (10\@+-2\@+ km\@+-1\@+)\" \\
+  system "gmt psscale -Dx$cpt_x/$cpt_y/$cpt_len/$cpt_width -Ba1f0.5g0.5+l\"Radiation pattern (10\@+-2\@+ km\@+-1\@+)\" \\
                        -C$ampterm_cpt -O -K >> $out";
 
   system "cat /dev/null | gmt psxy -JX1c -R0/1/0/1 -Sc0.1 -O >> $out";
